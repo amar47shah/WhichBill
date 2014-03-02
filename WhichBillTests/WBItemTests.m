03be2f2ac52a9d904da8979d635ecaa59d91995f
Amar Shah <amar47shah@gmail.com>
2014-03-02 00:30:16 -0500

    Remove auto-generated test class, add WBItemTests
    WhichBillTests.h and .m were created by Xcode and were not implemented.
    Tests were needed for all classes in application.
    
    * WhichBillTests.h and .m deleted
    * Empty WBItemTests.m created

---
+++ WhichBillTests/WBItemTests.m
@@ -0,0 +1,34 @@
+//
+//  WBItemTests.m
+//  WhichBill
+//
+//  Created by Amar Shah on 3/1/14.
+//  Copyright (c) 2014 Amar Shah. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WBItemTests : SenTestCase
+
+@end
+
+@implementation WBItemTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
